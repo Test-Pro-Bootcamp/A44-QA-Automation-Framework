10c90decaeedf75c315ae9a8b7a8614f33b9df92
AndreiBryliakov01 <andrei.bryliakov01@testpro.io>
2023-05-19 14:11:24 +0200

    Homework16AndreiBryliakov

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
+
+public class Homework16  {
+    @Test
+    public void registrationNavigation(){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notification*");
+
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+
+
+        WebElement submitRegistration = driver.findElement(By.cssSelector("a[type='submit']"));
+        submitRegistration.click();
+
+        WebElement post = driver.findElement(By.cssSelector("[method='POST']"));
+        Assert.assertTrue(post.isDisplayed());
+        driver.quit();
+
+
+    }
+}
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -6,20 +8,38 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
+ public class LoginTests extends BaseTest {
     @Test
-    public static void LoginEmptyEmailPasswordTest() {
+    public static void loginSucceedTest()throws  InterruptedException {
 
 //      Added ChromeOptions argument below to fix websocket error
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notification*");
+
 
         WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "testpro.io";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        String url = "https://bbb.testpro.io/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("andrei.bryliakov01@testpro.io");
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys("te$t$tudent");
+        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitLogin.click();
+
+        WebElement avatar = driver.findElement(By.cssSelector(".avatar"));
+        Assert.assertTrue(avatar.isDisplayed());
         driver.quit();
+
+
     }
+
+
 }
