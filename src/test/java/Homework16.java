fbaf5377ff97a0f2adf7e226dfdd1afa2442478e
LuckyCat26 <zhuldyzsyrlybayeva@gmail.com>
2023-05-19 13:57:52 -0500

    RegistationNavigation Test added

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 {
+    @Test
+    public static void LoginTest() {
+//      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        WebElement registration = driver.findElement(By.cssSelector("#hel"));
+        registration.click();
+
+        WebElement registrationButton = driver.findElement(By.cssSelector("[value='Register']"));
+        Assert.assertTrue(registrationButton.isDisplayed());
+        driver.quit();
+    }
+}
