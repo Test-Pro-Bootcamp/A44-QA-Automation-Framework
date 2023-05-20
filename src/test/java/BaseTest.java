6d4252cca965003daf5e36831f6ca594096c83f3
LuckyCat26 <zhuldyzsyrlybayeva@gmail.com>
2023-05-20 15:26:07 -0500

    updated gradle

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -7,8 +7,7 @@ import org.testng.annotations.BeforeSuite;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.TimeoutException;
+
 
 import java.time.Duration;
 
