4cb81b3d7329cfac37a3bec34f42e4342bf487c8
AndreyZlotnikov <andrey.zlotnikov@testpro.io>
2023-05-23 15:10:50 -0500

    Update Homework17.java

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,6 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 public class Homework17 extends BaseTest {
     @Test
     public void addSongToPlaylist() throws InterruptedException   {
@@ -8,20 +12,24 @@ public class Homework17 extends BaseTest {
         enterEmail("demo@class.com");
         enterPassword("te$t$tudent");
         clickLoginBtn();
-        enterToSearch("Epic Song");
+        //click View All button results
         WebElement viewAllBtn = driver.findElement(By.cssSelector("[data-test='view-all-songs-btn']"));
         viewAllBtn.click();
-        WebElement song = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//table[@class='items']/tr[1]"));
-        song.click();
+        //click first song from list
+        List<WebElement> songsInResults = driver.findElements(By.cssSelector(".search-results .items [draggable='true']:nth-of-type(1)"));
+        songsInResults.get(0).click();
+        // click Add To button
         WebElement addToBtn = driver.findElement(By.cssSelector("[title='Add selected songs to…']"));
         addToBtn.click();
+        //Creation of new playlist with selected song
         WebElement playlistNameFld = driver.findElement(By.cssSelector("#songResultsWrapper [required]"));
         playlistNameFld.click();
         playlistNameFld.sendKeys("Great Music_777");
         WebElement savePlaylistBtn = driver.findElement(By.cssSelector("#songResultsWrapper [type='submit']"));
         savePlaylistBtn.click();
-
-
+        //assertion - success banner
+        WebElement successBanner = driver.findElement(By.cssSelector(".success"));
+        Assert.assertTrue(successBanner.isDisplayed());
     }
        public void enterToSearch (String song) throws InterruptedException {
            WebElement searchLine = driver.findElement(By.cssSelector("[name='q']"));
