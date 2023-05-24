0a8209cd25848ef1419f220cc97ea51f8861c70c
AndreyZlotnikov <andrey.zlotnikov@testpro.io>
2023-05-23 19:44:59 -0500

    Update Registration.java

--- src/test/java/Registration.java
+++ src/test/java/Registration.java
@@ -15,28 +15,12 @@ public class Registration extends BaseTest {
         options.addArguments("--disable-notifications");
 
         WebDriver driver = new ChromeDriver(options);
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(17));
-
         String url = "https://bbb.testpro.io/";
         driver.get(url);
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(17));
         WebElement registrationButton = driver.findElement(By.cssSelector("[id='hel']"));
         registrationButton.click();
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(17));
         WebElement registerButton = driver.findElement(By.cssSelector("[id='button']"));
         registerButton.click();
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(17));
-
-
-        //registerButton.click();
-        //passwordInput.clear();
-        //passwordInput.sendKeys("te$t$tudent");
-        //WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
-        //submitLogin.click();
-        //WebElement avatar = driver.findElement(By.cssSelector(".avatar"));
-        //Assert.assertTrue(avatar.isDisplayed());
-        //Thread.sleep(5000);
-        //List<WebElement> playlists = driver.findElements(By.cssSelector("#playlists"));
         driver.quit();
     }
 }
