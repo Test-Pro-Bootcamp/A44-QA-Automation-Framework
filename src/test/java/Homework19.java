a82006076c3b7242a4bd53058dd3b53934b8e1be
AndreyZlotnikov <andrey.zlotnikov@testpro.io>
2023-05-26 00:14:30 -0500

    Homework19

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,21 +1,16 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class Homework19 extends BaseTest {
-    @BeforeMethod
-    @Parameters({"BaseURL"})
-    public void launchBrowser(String BaseURL) {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get(BaseURL);
-    }
+    //@BeforeMethod
+   // @Parameters({"BaseURL"})
+    //public void launchBrowser(String BaseURL) {
+        //driver = new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //driver.get(BaseURL);
+    //}
 
     @Test
     public void deletePlaylist() throws InterruptedException {
@@ -24,14 +19,19 @@ public class Homework19 extends BaseTest {
         enterPassword("te$t$tudent");
         clickLoginBtn();
         //Deletion of selected playlist
-        WebElement playlistToDelete = driver.findElement(By.cssSelector("[href='#!/playlist/58066']"));
+        WebElement playlistToDelete = driver.findElement(By.cssSelector("[href='#!/playlist/58068']"));
         playlistToDelete.click();
         WebElement deletePlaylistBtn = driver.findElement(By.cssSelector("[title='Delete this playlist']"));
         deletePlaylistBtn.click();
-        WebElement deleteNotification = driver.findElement(By.cssSelector("Delete the playlist 'Great Music_777'"));
+        WebElement deleteNotification = driver.findElement(By.cssSelector("[class='msg']"));
+        Thread.sleep(2000);
         Assert.assertTrue(deleteNotification.isDisplayed());
         WebElement deleteOKBtn = driver.findElement(By.cssSelector("button[class='ok']"));
         deleteOKBtn.click();
+        //Assertion Deleted playlist banner is displayed 
+        WebElement deleteBanner = driver.findElement(By.cssSelector("div[class='success show']"));
+        Thread.sleep(2000);
+        Assert.assertTrue(deleteBanner.isDisplayed());
 
     }
 }
