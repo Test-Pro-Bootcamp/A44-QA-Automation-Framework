26fa38cb2e100631a6cebd2396c8f8f4fdee50a6
AndreyZlotnikov <andrey.zlotnikov@testpro.io>
2023-05-25 23:28:57 -0500

    Update Homework19.java

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,6 +1,10 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -11,5 +15,23 @@ public class Homework19 extends BaseTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(BaseURL);
+    }
+
+    @Test
+    public void deletePlaylist() throws InterruptedException {
+        //Login with valid credentials
+        enterEmail("demo@class.com");
+        enterPassword("te$t$tudent");
+        clickLoginBtn();
+        //Deletion of selected playlist
+        WebElement playlistToDelete = driver.findElement(By.cssSelector("[href='#!/playlist/58066']"));
+        playlistToDelete.click();
+        WebElement deletePlaylistBtn = driver.findElement(By.cssSelector("[title='Delete this playlist']"));
+        deletePlaylistBtn.click();
+        WebElement deleteNotification = driver.findElement(By.cssSelector("Delete the playlist 'Great Music_777'"));
+        Assert.assertTrue(deleteNotification.isDisplayed());
+        WebElement deleteOKBtn = driver.findElement(By.cssSelector("button[class='ok']"));
+        deleteOKBtn.click();
+
     }
 }
