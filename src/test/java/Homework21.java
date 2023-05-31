828b4ef7fd61fb613e43864c4f3707fa1d776cd3
AndreyZlotnikov <andrey.zlotnikov@testpro.io>
2023-05-31 18:30:18 -0500

    Update Homework21.java

---
+++ src/test/java/Homework21.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class Homework21 extends BaseTest {
+    @Test
+    public void renamePlaylist(){
+        String newPlaylistName = "Excellent songs";
+        login("demo@class.com", "te$t$tudent");
+        
+
+        }
+        }
