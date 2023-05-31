97dde74b7036e492d7e2548cb8a1f34b370e886f
AndreyZlotnikov <andrey.zlotnikov@testpro.io>
2023-05-31 18:48:54 -0500

    Update Homework21.java

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -1,3 +1,6 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class Homework21 extends BaseTest {
@@ -5,7 +8,9 @@ public class Homework21 extends BaseTest {
     public void renamePlaylist(){
         String newPlaylistName = "Excellent songs";
         login("demo@class.com", "te$t$tudent");
-        
+        //
+        WebElement playlistElement = wait.until(ExpectedConditions.
+                elementToBeClickable(By.cssSelector(".playlist:nth-child(3)")));
 
         }
         }
