6b2d59ac3972597505016ac2d2df0c6499ac2e35
AndreyZlotnikov <andrey.zlotnikov@testpro.io>
2023-06-04 21:16:13 -0500

    Homework23

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class LoginPage extends BasePage {
@@ -10,9 +11,17 @@ public class LoginPage extends BasePage {
         super(givenDriver);
     }
 
-    By emailField = By.cssSelector("[type='email']");
-    By passwordField = By.cssSelector("[type='password']");
-    By submitLoginBtn = By.cssSelector("button[type='submit']");
+    @FindBy(css = "[type='email']")
+    WebElement emailField;
+    //By emailField = By.cssSelector("[type='email']");
+
+    @FindBy(css = "[type='password']")
+    WebElement passwordField;
+    //By passwordField = By.cssSelector("[type='password']");
+
+    @FindBy(css = "button[type='submit']")
+            WebElement submitLoginBtn;
+    //By submitLoginBtn = By.cssSelector("button[type='submit']");
 
 
     public void clickLoginBtn() {
@@ -34,7 +43,7 @@ public class LoginPage extends BasePage {
         emailInput.sendKeys(email);
     }
 
-    public void login(String email, String password){
+    public void login(String email, String password) {
         enterEmail(email);
         enterPassword(password);
         clickLoginBtn();
