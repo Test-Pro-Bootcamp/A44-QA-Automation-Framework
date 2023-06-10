5eba113451e718b543cfb077b9c5d5fd01e80882
AndreyZlotnikov <andrey.zlotnikov@testpro.io>
2023-06-10 17:10:11 -0500

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,25 +3,23 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.Locale;
 
 public class BaseTest {
+        private static final ThreadLocal<WebDriver> THREAD_LOCAL = new ThreadLocal<>();
     public static WebDriver driver = null;
-
     public static String url = "https://qa.koel.app/";
     public static WebDriverWait wait = null;
-
-    
+    public static WebDriver getThreadLocal() {
+        return THREAD_LOCAL.get();
+    }
 
 
 
@@ -31,20 +29,7 @@ public class BaseTest {
     }
 
     @BeforeMethod
-//    @Parameters({"BaseURL"})
-  //  public void setUpBrowser(String BaseURL){
-        public void setUpBrowser(){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--start-maximized");
-
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-
-        openUrl(url);
-    }
+    
 
     @AfterMethod(alwaysRun = true)
     public void tearDown(){
