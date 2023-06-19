12c96dc9ecc289c70eb60b537f6cb843c15c3be6
AndreyZlotnikov <andrey.zlotnikov@testpro.io>
2023-06-18 20:40:38 -0500

    Update StepDefinitions.java

--- src/test/java/stepDefinition/StepDefinitions.java
+++ src/test/java/stepDefinition/StepDefinitions.java
@@ -25,96 +25,52 @@ import java.net.URI;
 import java.time.Duration;
 
 public class StepDefinitions {
-    public static final ThreadLocal<WebDriver> THREAD_LOCAL = new ThreadLocal<>();
-    public static WebDriver driver = null;
 
-    public static WebDriver getThreadLocal() {
-        return THREAD_LOCAL.get();
-    }
 
-    @Before
-    public void openBrowser() throws MalformedURLException {
-        THREAD_LOCAL.set(pickBrowser(System.getProperty("browser")));
-        THREAD_LOCAL.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-    }
 
-    @After
-    public void tearDown(){
-        THREAD_LOCAL.get().close();
-        THREAD_LOCAL.remove();
-    }
 
-    public WebDriver pickBrowser(String browser) throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        String gridURL = "http://192.168.0.159:4444";
-        switch (browser){
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                return driver = new FirefoxDriver();
-            case "safari":
-                WebDriverManager.safaridriver().setup();
-                return driver = new SafariDriver();
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                return driver = new EdgeDriver();
-            case "grid-firefox":
-                capabilities.setCapability("browserName", "firefox");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
-            case "grid-safari":
-                capabilities.setCapability("browserName", "safari");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), capabilities);
-//            case "cloud":
-//                return lambdaTest();
-            default:
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--remote-allow-origins=*");
-                options.addArguments("--disable-notifications");
-                options.addArguments("--start-maximized");
-                // options.addArguments("--headless=new");
-                return driver = new ChromeDriver(options);
-        }
-    }
+
+
 
 
     @Given("I open Login Page")
     public void openLoginPage(){
-        getThreadLocal().get("https://qa.koel.app/");
+        driver.get("https://qa.koel.app/");
     }
 
     @When("I enter email")
     public void iEnterEmail() {
-        LoginPage loginPage = new LoginPage(getThreadLocal());
+        LoginPage loginPage = new LoginPage(driver);
         loginPage.enterEmail("demo@class.com");
     }
 
     @And("I enter password")
     public void iEnterPassword() {
-        LoginPage loginPage = new LoginPage(getThreadLocal());
+        LoginPage loginPage = new LoginPage(driver);
         loginPage.enterPassword("te$t$tudent");
     }
 
     @And("I click submit")
     public void iClickSubmit() {
-        LoginPage loginPage = new LoginPage(getThreadLocal());
+        LoginPage loginPage = new LoginPage(driver);
         loginPage.clickLoginBtn();
     }
 
-    @Then("I logged in")
-    public void iLoggedIn() {
-        HomePage homePage = new HomePage(getThreadLocal());
+    @Then("I am logged in")
+    public void iAmLoggedIn() {
+        HomePage homePage = new HomePage(driver);
         Assert.assertTrue(homePage.getAvatar());
     }
 
     @And("I click play button")
     public void clickPlayBtn(){
-        SongsPage songsPage = new SongsPage(getThreadLocal());
+        SongsPage songsPage = new SongsPage(driver);
         songsPage.startPlaySong();
     }
 
     @Then("I see equalizer")
     public void iSeeEqualizer() {
-        SongsPage songsPage = new SongsPage(getThreadLocal());
+        SongsPage songsPage = new SongsPage(driver);
         Assert.assertTrue(songsPage.isEqualizerDisplayed());
     }
 }
