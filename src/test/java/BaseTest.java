a13eba4aba8bcb628aacc5301eb10b10e27c7673
Grigore24 <133910092+Grigore24@users.noreply.github.com>
2023-06-28 11:26:04 +0100

    Homework20up

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,23 +1,26 @@
+import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Parameters;
 
-
 import java.time.Duration;
-
+import java.util.List;
+import java.util.Locale;
 
 public class BaseTest {
     static WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeSuite
     static void setupDriver() {
@@ -25,22 +28,64 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    @Parameters({"BaseURL"})
-
-    public void setUpBrowser(String BaseURL) {
+//    @Parameters({"BaseURL"})
+    //  public void setUpBrowser(String BaseURL){
+    public void setUpBrowser(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--disable-notifications");
+        options.addArguments("--start-maximized");
 
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        String url=BaseURL;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        String url = "https://qa.koel.app/";
         openUrl(url);
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(){
         driver.quit();
+    }
+
+    public String generateRandomName(){
+        Faker faker = new Faker(new Locale("en-US"));
+        String newName = faker.name().firstName();
+        return newName;
+    }
+
+    public String generateRandomPlaylistName(){
+        Faker faker = new Faker(new Locale("en-US"));
+        String newName = faker.address().country();
+        return newName;
+    }
+
+    protected void clickLoginBtn() {
+        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitLogin.click();
+    }
+
+    public void enterPassword(String password) {
+        WebElement passwordInput = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[type='password']")));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys(password);
+    }
+
+    protected void enterEmail(String email) {
+        WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[type='email']")));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys(email);
+    }
+
+    public WebElement waitUntilVisible(By element){
+        return new WebDriverWait(driver, Duration.ofSeconds(4)).until(ExpectedConditions.visibilityOfElementLocated(element));
+    }
+
+    public WebElement waitUntilClickable(By element){
+        return new WebDriverWait(driver, Duration.ofSeconds(4)).until(ExpectedConditions.elementToBeClickable(element));
     }
 
 
@@ -48,110 +93,58 @@ public class BaseTest {
         driver.get(url);
     }
 
-    public void clickLoginBtn() {
-        WebElement submitLogin = driver.findElement(By.cssSelector("button[type='submit']"));
-        submitLogin.click();
-    }
 
-    public void enterPassword(String password) {
-        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
-        passwordInput.click();
-        passwordInput.clear();
-        passwordInput.sendKeys(password);
-    }
-
-    public void enterEmail(String email) {
-        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
-        emailInput.click();
-        emailInput.clear();
-        emailInput.sendKeys(email);
-    }
-
-   /* public void searchSong(String song) {
-        WebElement searchInput = driver.findElement(By.cssSelector("[type='search']"));
-        searchInput.click();
-        searchInput.clear();
-        searchInput.sendKeys(song);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
-
-    public void clickViewAllBtn() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        WebElement viewAllBtn = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
-        js.executeScript("arguments[0].click();", viewAllBtn);
-
-    }
-
-    /*public void clickFirstSong() {
-        WebElement searchSong = driver.findElement(By.cssSelector("[class='song-list-wrap main-scroll-wrap search-results'] .items"));
-        searchSong.click();
-    }*/
-
-    public void waitCommand() {
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
-
-    /*public void clickAddToBtn() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        WebElement addToBtn = driver.findElement(By.cssSelector(".btn-add-to"));
-        js.executeScript("arguments[0].click();", addToBtn);
-
-    }
-
-    public void playlistNameInput(String playlistName) {
-        WebElement nameInput = driver.findElement(By.cssSelector("[id='songResultsWrapper'] [placeholder='Playlist name']"));
-        nameInput.click();
-        nameInput.clear();
-        nameInput.sendKeys(playlistName);
-    }*/
-
-   /* public void clickSave() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        WebElement saveBtn = driver.findElement(By.cssSelector("[id='songResultsWrapper'] [type='submit']"));
-        js.executeScript("arguments[0].click();", saveBtn);
-
-        WebElement successBanner = driver.findElement(By.cssSelector("[class='success show']"));
-        Assert.assertTrue(successBanner.isDisplayed());
-    }*/
-    public void login(String email, String password) {
-        //openUrl();
+    public void login(String email, String password){
         enterEmail(email);
         enterPassword(password);
         clickLoginBtn();
     }
 
-   /* public void clickPlayBtn() {
-        WebElement playBtn = driver.findElement(By.cssSelector("[data-testid='play-btn']"));
+    public void searchForSong(String text) {
+        WebElement searchInput = driver.findElement(By.cssSelector("[type='search']"));
+        searchInput.click();
+        searchInput.clear();
+        searchInput.sendKeys(text);
+    }
+
+    public String getSongName(){
+        WebElement songName = driver.findElement(By.cssSelector("#playlistWrapper .song-item .title"));
+        String songText = songName.getText();
+        return songText;
+    }
+
+    public boolean isBannerDisplayed(){
+        WebElement successBanner = driver.findElement(By.cssSelector(".success"));
+        return successBanner.isDisplayed();
+    }
+
+    public void createNewPlaylistWhileAddingSong(String playlistName) {
+        WebElement newPlaylistNameInput = driver.findElement(By.cssSelector("[id='songResultsWrapper'] [placeholder='Playlist name']"));
+        newPlaylistNameInput.click();
+        newPlaylistNameInput.clear();
+        newPlaylistNameInput.sendKeys(playlistName);
+        newPlaylistNameInput.click();
+        newPlaylistNameInput.clear();
+        newPlaylistNameInput.sendKeys(playlistName);
+        // click Enter
         new Actions(driver)
-                .moveToElement(playBtn)
+                .keyDown(Keys.ENTER)
                 .perform();
-        playBtn.click();
     }
 
-    /* public void assertPauseBtn() {
-        WebElement pauseBtn = driver.findElement(By.cssSelector("[data-testid='pause-btn']"));
-        Assert.assertTrue(pauseBtn.isDisplayed());
-    }*/
-
-    public void showSuccessBanner() {
-        WebElement successBanner = driver.findElement(By.cssSelector("[class='success show']"));
-        Assert.assertTrue(successBanner.isDisplayed());
+    public void clickAddToPlaylistBtn() {
+        WebElement addToBtn = driver.findElement(By.cssSelector("[data-test='add-to-btn']"));
+        addToBtn.click();
     }
 
-    public void clickOkBtn() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        WebElement okBtn = driver.findElement(By.cssSelector(".ok"));
-        js.executeScript("arguments[0].click();", okBtn);
+    public void clickFirstSearchResultSong() {
+        List<WebElement> songsInResults = driver.findElements(By.cssSelector(".search-results .song-item .title"));
+        songsInResults.get(0).click();
     }
 
-    public void deleteSelectedPlaylist() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        WebElement deletePlaylistBtn = driver.findElement(By.cssSelector("[title='Delete this playlist']"));
-        js.executeScript("arguments[0].click();", deletePlaylistBtn);
+    public void clickViewAllBtn() {
+        WebElement viewAllBtn = driver.findElement(By.xpath("//button[@data-test='view-all-songs-btn']"));
+        viewAllBtn.click();
     }
 
-    public void clickPlaylist() {
-        WebElement playlist = driver.findElement(By.cssSelector("[class='playlist playlist']"));
-        playlist.click();
-    }
 }
