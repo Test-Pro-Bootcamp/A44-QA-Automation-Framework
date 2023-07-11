9d531c1495e683d7a55db936ab5ca9470b4707e7
Grigore24 <133910092+Grigore24@users.noreply.github.com>
2023-07-11 15:24:53 +0100

    Homework23PageFactory

--- src/test/java/Homework23.java
+++ src/test/java/Homework23.java
@@ -3,17 +3,14 @@ import org.testng.annotations.Test;
 import pages.ActionsPage;
 import pages.LoginPage;
 
-public class Homework22 extends BaseTest {
+public class Homework23 extends BaseTest {
     @Test
     public void renamePlaylist() {
         ActionsPage actionsPage =new ActionsPage(driver);
         LoginPage loginPage = new LoginPage(driver);
-        // double click
         String playlistName = "Happy Days";
-
         loginPage.login("grigore.crepciuc@testpro.io", "te$t$tudent");
-        actionsPage. doubleClickChoosePlaylist();
-        actionsPage.enterPlaylistName(playlistName);
+        actionsPage. doubleClickChoosePlaylist().enterPlaylistName(playlistName);
         String newName = actionsPage.getPlaylistName();
         Assert.assertEquals(playlistName, newName, "=== PlaylistNames expected to be equals ===");
     }
--- src/test/java/pages/ActionsPage.java
+++ src/test/java/pages/ActionsPage.java
@@ -5,26 +5,34 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ActionsPage extends BasePage{
     public ActionsPage(WebDriver givenDriver) {
         super(givenDriver);
     }
-    By doubleClick = By.cssSelector(".playlist:nth-child(3)");
-    By enterName = By.cssSelector("input[name='name']");
-    By getName = By.cssSelector(".playlist:nth-child(3)>a");
-    public void doubleClickChoosePlaylist() {
-        WebElement playlistElement = wait.until(ExpectedConditions.
-                elementToBeClickable(doubleClick));
+    private @FindBy(css = ".playlist:nth-child(3)")
+    WebElement playlistElement;
+    //By doubleClick = By.cssSelector(".playlist:nth-child(3)");
+    private @FindBy(css= "input[name='name']")
+    WebElement playlistInputField;
+    //By enterName = By.cssSelector("input[name='name']");
+
+    private By getName = By.cssSelector(".playlist:nth-child(3)>a");
+    public ActionsPage doubleClickChoosePlaylist() {
+       // WebElement playlistElement = wait.until(ExpectedConditions.
+            //    elementToBeClickable(doubleClick));
         Actions actions = new Actions(driver);
         actions.doubleClick(playlistElement).perform();
+        return this;
     }
 
-    public void enterPlaylistName(String name) {
-        WebElement playlistInputField = driver.findElement(enterName);
+    public ActionsPage enterPlaylistName(String name) {
+        //WebElement playlistInputField = driver.findElement(enterName);
         playlistInputField.sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), name);
         playlistInputField.sendKeys(Keys.ENTER);
+        return this;
     }
 
     public String getPlaylistName() {
